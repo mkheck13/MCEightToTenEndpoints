1316bf8a924d0b85846e6b190a9b168325b2ba4a
mkheck13 <mkheck13@gmail.com>
2025-01-09 14:04:53 -0800

    Guess it working

--- Controllers/GuessItController.cs
+++ Controllers/GuessItController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MCEightToTenEndpoints.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MCEightToTenEndpoints.Controllers
@@ -10,6 +7,31 @@ namespace MCEightToTenEndpoints.Controllers
     [Route("[controller]")]
     public class GuessItController : ControllerBase
     {
-        
+        private readonly GuessItServices _guessItServices;
+        public GuessItController(GuessItServices guessItServices)
+        {
+            _guessItServices = guessItServices;
+        }
+
+        [HttpGet]
+        [Route("EasyMode/{input}")]
+        public string EasyMode(string input)
+        {
+            return _guessItServices.EasyMode(input);
+        }
+
+        [HttpGet]
+        [Route("MedMode/{input}")]
+        public string MedMode(string input)
+        {
+            return _guessItServices.MedMode(input);
+        }
+
+        [HttpGet]
+        [Route("HardMode/{input}")]
+        public string HardMode(string input)
+        {
+            return _guessItServices.HardMode(input);
+        }
     }
 }
--- Services/GuessItServices.cs
+++ Services/GuessItServices.cs
@@ -2,6 +2,85 @@ namespace MCEightToTenEndpoints.Services
 {
     public class GuessItServices
     {
-        
+        public string EasyMode(string input)
+        {
+            int randomIndex = new Random().Next(0, 11);
+            bool isNum = int.TryParse(input, out int num);
+
+            if (isNum && num > 0 && num < 11)
+            {
+                if (num == randomIndex)
+                {
+                    return $"{num} was the correct guess";
+                }
+                else if (num > randomIndex)
+                {
+                    return $"{num} seems to be to high. Try again.";
+                }
+                else
+                {
+                    return $"{num} seems to be to low. Try again.";
+                }
+            }
+            else
+            {
+                return $"{input} was not a vailid input. Try guessing between 1 - 10.";
+            }
+
+        }
+
+        public string MedMode(string input)
+        {
+            int randomIndex = new Random().Next(0, 51);
+            bool isNum = int.TryParse(input, out int num);
+
+            if (isNum && num > 0 && num < 51)
+            {
+                if (num == randomIndex)
+                {
+                    return $"{num} was the correct guess";
+                }
+                else if (num > randomIndex)
+                {
+                    return $"{num} seems to be to high. Try again.";
+                }
+                else
+                {
+                    return $"{num} seems to be to low. Try again.";
+                }
+            }
+            else
+            {
+                return $"{input} was not a vailid input. Try guessing between 1 - 50.";
+            }
+
+        }
+
+        public string HardMode(string input)
+        {
+            int randomIndex = new Random().Next(0, 101);
+            bool isNum = int.TryParse(input, out int num);
+
+            if (isNum && num > 0 && num < 101)
+            {
+                if (num == randomIndex)
+                {
+                    return $"{num} was the correct guess";
+                }
+                else if (num > randomIndex)
+                {
+                    return $"{num} seems to be to high. Try again.";
+                }
+                else
+                {
+                    return $"{num} seems to be to low. Try again.";
+                }
+            }
+            else
+            {
+                return $"{input} was not a vailid input. Try guessing between 1 - 100.";
+            }
+
+        }
     }
 }
